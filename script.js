4d9d38bc82e5a30cd1faa38358376444d4643c7b
Kartz_Manager <decten.kartz@gmail.com>
2025-03-04 19:59:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,21 +5,28 @@ screen.disabled =true;
 
 function pressed(val){
     screen.value +=val;
+    aud.src = "press.mp3";
     aud.play();
 }
 
 function result(){
   if(screen.value.length != 0){
     screen.value = eval(screen.value);
+    aud.src = "result.mp3" ;
+    aud.play();
   }
 }
 
 function clr(){
     screen.value = "";
+    aud.src = "erase.mp3";
+    aud.play();
 }
 
 function del(){
   screen.value = screen.value.slice(0,screen.value.length-1);
+    aud.src = "erase.mp3";
+    aud.play();
 }
 
 
